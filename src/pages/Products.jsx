e1bcda14c7726a385f5ceccb9a6f30f9179fe264
Samankhan <123460007+SamanKhanDev@users.noreply.github.com>
2025-01-31 23:53:34 +0500

    new

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -7,18 +7,23 @@ const UpworkProfiles = () => {
   ];
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "20px" }}>
+    <div className="p-8">
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "40px" }}>
       {upworkLinks.map((profile, index) => (
         <div
           key={index}
           style={{
             border: "1px solid #ccc",
             padding: "15px",
-            width: "250px",
+            width: "450px",
             borderRadius: "10px",
             textAlign: "center",
             transition: "transform 0.3s, box-shadow 0.3s",
             boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+            backgroundImage: "rl('https://cdn2.iconfinder.com/data/icons/picons-social/57/79-upwork-2-1024.png')",
+            backgroundSize: "cover",
+            backgroundRepeat: "repeat",
+            backgroundPosition: "center",
           }}
           onMouseEnter={(e) => {
             e.currentTarget.style.transform = "scale(1.05)";
@@ -29,7 +34,7 @@ const UpworkProfiles = () => {
             e.currentTarget.style.boxShadow = "0px 4px 6px rgba(0, 0, 0, 0.1)";
           }}
         >
-         <div className="flex justify-items-center items-center">
+         <div className="flex justify-between items-center">
          <div><img src={profile.image} alt={`Upwork Profile ${index + 1}`} style={{ width: "100px", borderRadius: "50%" }} /></div>
          <div> <h3 style={{ margin: "10px 0" }}>Upwork Profile</h3>
           <a
@@ -43,6 +48,7 @@ const UpworkProfiles = () => {
          </div>
         </div>
       ))}
+    </div>
     </div>
   );
 };
